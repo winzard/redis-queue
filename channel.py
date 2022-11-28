4fe4aa0cf08d37281620ece77d42697a88cd0263
maksim.vasenkov@eteron.ru <maksim.vasenkov@eteron.ru>
2022-11-28 13:52:34 +0300

    пофиксил обработку change при первом запуске

--- channel.py
+++ channel.py
@@ -22,21 +22,21 @@ class Channel:
 
     def __init__(self, name, group_name) -> None:
         super().__init__()
-        self.r = redis.Redis(host=REDIS_HOST, port=6379, db=0)
+        self.transport = redis.Redis(host=REDIS_HOST, port=6379, db=0)
         self.name = name
         self.group_name = group_name
         self.last_seen = '>'
         try:
-            self.r.xgroup_create(name, group_name, mkstream=True)
+            self.transport.xgroup_create(name, group_name, mkstream=True)
         except:
             pass
 
     def acknowledge(self, message_id):
-        self.r.xack(self.name, self.group_name, message_id)
+        self.transport.xack(self.name, self.group_name, message_id)
 
     def is_pending(self, message_id):
-        pending_entries = self.r.xpending_range(self.name, self.group_name, message_id, message_id,
-                                                1)  # а вот без консумера, consumername=consumername)
+        pending_entries = self.transport.xpending_range(self.name, self.group_name, message_id, message_id,
+                                                        1)  # а вот без консумера, consumername=consumername)
         if pending_entries:
             return True
         else:
@@ -48,16 +48,16 @@ class Channel:
         :param consumer_id: id worker
         :return:
         """
-        self.r.xclaim(self.name, self.group_name, consumer_id, WORKER_TIMEOUT_MILLISECONDS, ['1'])
+        self.transport.xclaim(self.name, self.group_name, consumer_id, WORKER_TIMEOUT_MILLISECONDS, ['1'])
 
     def send(self, recipient_channel, message):
-        self.r.xadd(recipient_channel, message)
+        self.transport.xadd(recipient_channel, message)
 
     def _dispatch(self, consumer_id, message_id, command) -> bool:
         command_id = command['id']
         tenant_id = command['tenant_id']
-        pending_entries = self.r.xpending_range(self.name, self.group_name, '-', message_id,
-                                                MESSAGE_QUEUE_SIZE)
+        pending_entries = self.transport.xpending_range(self.name, self.group_name, '-', message_id,
+                                                        MESSAGE_QUEUE_SIZE)
         if not pending_entries:
             return False
         print(f'Всего сообщений {len(pending_entries)}')
@@ -69,10 +69,10 @@ class Channel:
                 logger(f,
                        f'{consumer_id} проверяет сообщение {request_id} для обработчика {other_worker}')
 
-            rng = self.r.xrange(self.name, request_id, request_id, count=1)
+            rng = self.transport.xrange(self.name, request_id, request_id, count=1)
             if not rng:
                 print(f'непонятно, почему нет сообщения')
-                self.r.xack(self.name, self.group_name, request_id)
+                self.transport.xack(self.name, self.group_name, request_id)
                 continue
             _message_id, entry = rng[0]
             for _command_id in entry:
@@ -80,7 +80,7 @@ class Channel:
                 _tenant_id = _command['tenant_id']
                 if consumer_id != other_worker.decode('utf-8') and _command_id != command_id and tenant_id == _tenant_id and \
                         _command['type'] != 'query':  # кто-то уже обрабатывает команду для этого тенанта и это не запрос
-                    self.r.xclaim(self.name, self.group_name, other_worker, 1, [message_id])
+                    self.transport.xclaim(self.name, self.group_name, other_worker, 1, [message_id])
                     for f in [None, 'worker.txt', f"worker-{self.name}-{consumer_id}.txt"]:
                         logger(f,
                                f'{consumer_id} обработчик передает обработку {request_id} {command_id} для тенанта {tenant_id} обработчику {other_worker}, потому что он уже обрабатывает команду {_command_id}')
@@ -100,7 +100,7 @@ class Channel:
 
     def get_message_for(self, consumer_id):
         print('обработка зависших')
-        consumers = self.r.xinfo_consumers(self.name, self.group_name)
+        consumers = self.transport.xinfo_consumers(self.name, self.group_name)
         for info in consumers:
             idle_time = info['idle']
             pending_messages = info['pending']
@@ -108,27 +108,27 @@ class Channel:
             if pending_messages == 0 and idle_time > WORKER_TIMEOUT_MILLISECONDS:
                 for f in [None, "worker.txt", f"worker-{self.name}-{consumer_id}.txt"]:
                     logger(f, f'{consumer_id} обработчик удаляет отвалившийся обработчик {consumer}')
-                self.r.xgroup_delconsumer(self.name, self.group_name, consumer)
+                self.transport.xgroup_delconsumer(self.name, self.group_name, consumer)
             if pending_messages and idle_time > COMMAND_TIMEOUT_MILLISECONDS:
                 # забираем себе закисшие команды. TODO а что будет, если зависли команды разных тенантов?
                 # self.r.xautoclaim(self.name, self.group_name, consumer_id, COMMAND_TIMEOUT_MILLISECONDS)
                 # autoclaim не подходит, т.к. мы хотим забрать не все зависшие, а только для определенного воркера
-                entries = self.r.xpending_range(self.name, self.group_name, '-', '+', MESSAGE_QUEUE_SIZE,
-                                                consumername=consumer)
+                entries = self.transport.xpending_range(self.name, self.group_name, '-', '+', MESSAGE_QUEUE_SIZE,
+                                                        consumername=consumer)
                 message_ids = [message['message_id'] for message in entries]
                 if message_ids:
-                    self.r.xclaim(self.name, self.group_name, consumer_id, COMMAND_TIMEOUT_MILLISECONDS, message_ids)
+                    self.transport.xclaim(self.name, self.group_name, consumer_id, COMMAND_TIMEOUT_MILLISECONDS, message_ids)
                     for f in [None, 'worker.txt', f"worker-{self.name}-{consumer_id}.txt"]:
                         logger(f,
                                f'{consumer_id} обработчик забирает себе обработку сообщений {message_ids} обработчика {consumer}, потому что он упал')
                     break # забираем только у одного консумера, и хватит забирать лишнее, это бы успеть обработать
         print('обработка переданных')
-        entries = self.r.xpending_range(self.name, self.group_name, '-', '+', 1,
-                                        consumername=consumer_id)  # по одному, потому что может там отвис обработчик
+        entries = self.transport.xpending_range(self.name, self.group_name, '-', '+', 1,
+                                                consumername=consumer_id)  # по одному, потому что может там отвис обработчик
 
         for message in entries:  # todo time_since_delivered
             request_id = message['message_id'].decode("utf-8")
-            entries = self.r.xrange(self.name, request_id, request_id)
+            entries = self.transport.xrange(self.name, request_id, request_id)
             if entries:
                 for message_id, entry in entries:
                     for command_id in entry:
@@ -137,11 +137,11 @@ class Channel:
             else:
                 for f in [None, "worker.txt", f"worker-{self.name}-{consumer_id}.txt"]:
                     logger(f, f'{consumer_id} обработчик удаляет сообщение {request_id}')
-                self.r.xdel(self.name, request_id)
+                self.transport.xdel(self.name, request_id)
         print(f'новый цикл {self.last_seen}')
 
-        entries = self.r.xreadgroup(self.group_name, consumer_id, {self.name: self.last_seen}, count=1,
-                                    block=BLOCK_TIME_MILLISECONDS)  # or block None??
+        entries = self.transport.xreadgroup(self.group_name, consumer_id, {self.name: self.last_seen}, count=1,
+                                            block=BLOCK_TIME_MILLISECONDS)  # or block None??
         # полученные сообщения попадают в PEL и другие консумеры считают, что этот воркер их обрабатывает
         print(entries)
         if entries:
--- client.py
+++ client.py
@@ -8,7 +8,6 @@ $ python3 client.py A
 import json
 import os
 import random
-import uuid
 from copy import copy
 from datetime import datetime
 from pprint import pprint
@@ -19,7 +18,7 @@ import sys
 
 
 REDIS_HOST = os.environ.get('REDIS_HOST', 'localhost')
-r = redis.Redis(host=REDIS_HOST, port=6379, db=0)
+
 
 command1 = {
     'id': None,
@@ -49,14 +48,27 @@ def logger(filename, text):
 
 
 class Client:
-    def __init__(self, subsystem: str, client_id: str):
-        self.subsystem = subsystem
-        self.client_id = client_id
+    # def __init__(self, subsystem: str, client_id: str):
+    #     self.subsystem = subsystem
+    #     self.client_id = client_id
+    def __init__(self, name, group_name) -> None:
+        super().__init__()
+        self.transport = redis.Redis(host=REDIS_HOST, port=6379, db=0)
+        self.name = name
+        self.group_name = group_name
+        self.last_seen = '$'
+        # try:
+        #     self.transport.xgroup_create(name, group_name, mkstream=True)
+        # except:
+        #     pass
+
+    def send(self, recipient_channel, message):
+        self.transport.xadd(recipient_channel, message)
 
     def got_expected_response(self, entries, command_id):
         message_id = None
         for message_id, entry in entries:
-            r.xdel(f"response-{self.client_id}", message_id)
+            self.transport.xdel(f"response-{self.group_name}", message_id)
             for event_id in entry:
                 print(event_id.decode("utf-8"))
                 print(command_id)
@@ -72,43 +84,40 @@ class Client:
         # получить id последней записи из очереди events-tenant1
         # в принципе, он может как-нибудь по другому поддерживать
         # id последнего события, которое он видел
-        try:
-            r.xgroup_create('events', self.client_id, mkstream=True)  # будем подтверждать чтение?
-        except:
-            pass
 
-        last_seen = '$'
+
+
 
         if command['type'] == 'query':
-            counter = r.incr(f'query-{tenant_id}', 1)
+            counter = self.transport.incr(f'query-{tenant_id}', 1)
             command['id'] = f'query-{tenant_id}-{counter}'
         else:
-            counter = r.incr(f'command-{tenant_id}', 1)
+            counter = self.transport.incr(f'command-{tenant_id}', 1)
             command['id'] = f'command-{tenant_id}-{counter}'
         command['tenant_id'] = tenant_id
-        command['response-to'] = f"response-{self.client_id}"
+        command['response-to'] = f"response-{self.group_name}"
         command['params']['argument'] = counter
         start = datetime.now()
-        for f in [None, f"{tenant_id}.txt", f"client-{self.client_id}.txt"]:
-            logger(f, f"client-{self.client_id} S {start} {command['id']} {command['name']}")
-        message_id = r.xadd(self.subsystem, {command['id']: json.dumps(command)})
+        for f in [None, f"{tenant_id}.txt", f"client-{self.group_name}.txt"]:
+            logger(f, f"client-{self.group_name} S {start} {command['id']} {command['name']}")
+        message_id = self.send(self.name, {command['id']: json.dumps(command)})
 
         # считывать ответы после last_id
         # когда мы видим событие, которое ждём, то можно выходить
         while True:  # TODO возможно, нужно выгребать старые сообщения тоже, плюс будет нужен таймаут
             print('wait for response')
-            read = r.xread({f"response-{self.client_id}": last_seen}, count=1, block=0)  # or timeout?
+            read = self.transport.xread({f"response-{self.group_name}": self.last_seen}, count=1, block=0)  # or timeout?
             if read:
                 _, entries = read[0]
                 # entries = r.xrange(f"response-{client_id}", '-', '+', count=1)
                 result, last_seen = self.got_expected_response(entries, command['id'])
                 if result is not None:
                     end = datetime.now()
-                    for f in [None, f"{tenant_id}.txt", f"client-{self.client_id}.txt"]:
-                        logger(f, f"client-{self.client_id} F {end} {command['id']} {command['name']} {result}")
-                    # r.xgroup_destroy('events', self.client_id)
+                    for f in [None, f"{tenant_id}.txt", f"client-{self.group_name}.txt"]:
+                        logger(f, f"client-{self.group_name} F {end} {command['id']} {command['name']} {result}")
+
                     return {
-                        'client_id': f"client-{self.client_id}",
+                        'client_id': f"client-{self.group_name}",
                         'command_id': command['id'],
                         'command_name': command['name'],
                         'result': result
@@ -119,11 +128,6 @@ class Client:
         # получить id последней записи из очереди events-tenant1
         # в принципе, он может как-нибудь по другому поддерживать
         # id последнего события, которое он видел
-        try:
-            r.xgroup_create('events', self.client_id, mkstream=True)  # будем подтверждать чтение?
-        except:
-            pass
-
         last_seen = '$'
 
         for x in range(0, 10):
@@ -131,36 +135,35 @@ class Client:
             command = copy(random.choice([command1, command2]))
             tenant_id = random.choice(['tenant_1', 'tenant_2', 'tenant_3'])
             if command['type'] == 'query':
-                counter = r.incr(f'query-{tenant_id}', 1)
+                counter = self.transport.incr(f'query-{tenant_id}', 1)
                 command['id'] = f'query-{tenant_id}-{counter}'
             else:
-                counter = r.incr(f'command-{tenant_id}', 1)
+                counter = self.transport.incr(f'command-{tenant_id}', 1)
                 command['id'] = f'command-{tenant_id}-{counter}'
             command['tenant_id'] = tenant_id
-            command['response-to'] = f"response-{self.client_id}"
+            command['response-to'] = f"response-{self.group_name}"
             command['params']['argument'] = counter
             start = datetime.now()
-            for f in [None, f"{tenant_id}.txt", f"client-{self.client_id}.txt"]:
-                logger(f, f"client-{self.client_id} S {start} {command['id']} {command['name']}")
-            message_id = r.xadd(self.subsystem, {command['id']: json.dumps(command)})
+            for f in [None, f"{tenant_id}.txt", f"client-{self.group_name}.txt"]:
+                logger(f, f"client-{self.group_name} S {start} {command['id']} {command['name']}")
+            message_id = self.send(self.name, {command['id']: json.dumps(command)})
 
             # считывать ответы после last_id
             # когда мы видим событие, которое ждём, то можно выходить
             while True:  # TODO возможно, нужно выгребать старые сообщения тоже, плюс будет нужен таймаут
                 print(f'wait for response on {message_id}')
-                read = r.xread({f"response-{self.client_id}": last_seen}, count=1, block=0)  # or timeout?
+                read = self.transport.xread({f"response-{self.group_name}": last_seen}, count=1, block=0)  # or timeout?
                 if read:
                     _, entries = read[0]
-                    # entries = r.xrange(f"response-{client_id}", '-', '+', count=1)
                     result, last_seen = self.got_expected_response(entries, command['id'])
                     if result is not None:
                         end = datetime.now()
-                        for f in [None, f"{tenant_id}.txt", f"client-{self.client_id}.txt"]:
-                            logger(f, f"client-{self.client_id} F {end} {command['id']} {command['name']} {result}")
+                        for f in [None, f"{tenant_id}.txt", f"client-{self.group_name}.txt"]:
+                            logger(f, f"client-{self.group_name} F {end} {command['id']} {command['name']} {result}")
                         break
                     sleep(0.1)
-        r.xgroup_destroy('events', self.client_id)
+
 
 
 if __name__ == '__main__':
-    Client(subsystem=str(sys.argv[1]), client_id=str(sys.argv[2])).routine()
+    Client(name=str(sys.argv[1]), group_name=str(sys.argv[2])).routine()
